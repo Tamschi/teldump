23bbfcb026a344f10f9a667d0eae0116663726eb
Tamme Schichler <tamme@schichler.dev>
2021-05-21 21:01:11 +0200

    This works nicely

--- src/main.rs
+++ src/main.rs
@@ -7,19 +7,21 @@ pub mod readme {
 	doc_comment::doctest!("../README.md");
 }
 
-use std::time::Duration;
+use std::{io::Write, time::Duration};
 
 use telnet::{Telnet, TelnetEvent};
 
 fn main() {
-	let mut address = 0x8000_0000_i64;
+	// let mut address = 0xa000_0000_i64; // RAM
+	// let mut address = 0x8000_0000_i64;
+	let mut address = 0xBF_C0_00_00_i64; // Boot ROM
 	let mut read_size = 0x1000_i64;
 
 	address -= read_size;
 
 	'reconnect: while read_size > 0 {
 		eprint!("Connecting....");
-		let mut telnet = match Telnet::connect(("192.168.1.1", 23), 256) {
+		let mut telnet = match Telnet::connect(("192.168.1.1", 23), 0x1_00_00_00) {
 			Ok(telnet) => {
 				eprintln!("OK");
 				telnet
@@ -43,7 +45,10 @@ fn main() {
 				}
 			} {
 				TelnetEvent::Data(data) => {
-					print!("{}", String::from_utf8_lossy(&*data))
+					no_data = 0;
+					print!("{}", String::from_utf8_lossy(&*data));
+					std::io::stdout().flush().unwrap();
+					eprint!("Read. ")
 				}
 				TelnetEvent::UnknownIAC(_) => {
 					todo!("UnknownIAC");
@@ -60,11 +65,12 @@ fn main() {
 					continue 'reconnect;
 				}
 				TelnetEvent::NoData => {
+					eprint!(".");
 					no_data += 1;
 					if no_data >= 10 {
 						no_data = 0;
 
-						eprint!("Requesting more data: ");
+						eprint!("\nRequesting more data: ");
 						address += read_size;
 						let query = format!("memory {:#x} 8 {:#x}\r\n", address, read_size);
 						eprint!("{}", query);
@@ -77,10 +83,12 @@ fn main() {
 								eprintln!("Could not write. The device likely reset due to an out-of-bounds read. Retrying with smaller read size.");
 								address -= read_size;
 								read_size /= 2;
+								address += read_size;
+								continue 'reconnect;
 							}
 						}
 					} else {
-						std::thread::sleep(Duration::from_millis(5))
+						std::thread::sleep(Duration::from_millis(1))
 					}
 				}
 				TelnetEvent::Error(_) => {}
