fb03d6d5e44812aeb4baac6ccb87f69b9b289409
Tamme Schichler <tamme@schichler.dev>
2021-05-22 18:27:00 +0200

    Coarsely scan which memory is readable

--- src/main.rs
+++ src/main.rs
@@ -8,13 +8,15 @@ pub mod readme {
 }
 
 use std::{io::Write, time::Duration};
-
 use telnet::{Telnet, TelnetEvent};
 
 fn main() {
+	// return checkmem();
+
 	// let mut address = 0xa000_0000_i64; // RAM
 	// let mut address = 0x8000_0000_i64;
-	let mut address = 0xBF_C0_00_00_i64; // Boot ROM
+	// let mut address = 0xBF_C0_00_00_i64; // Boot ROM
+	let mut address = 0xBF_00_00_00_i64;
 	let mut read_size = 0x1000_i64;
 
 	address -= read_size;
@@ -100,4 +102,51 @@ fn main() {
 		"Read size has decreased to {}. Exiting. Have a good day.",
 		read_size
 	);
+}
+
+fn checkmem() {
+	let base = 0xA0_00_00_00_i64; // Unmapped uncached kseg1.
+	let test_range = 0_i64..0x2000_0000;
+	let step = 0x0100_0000;
+
+	'next_address: for address in test_range.step_by(step) {
+		eprintln!();
+		eprint!("{:#x}", address);
+		let mut telnet = loop {
+			match Telnet::connect(("192.168.1.1", 23), 0x1_00_00_00) {
+				Ok(telnet) => {
+					break telnet;
+				}
+				Err(_) => {
+					const WAIT: Duration = Duration::from_secs(1);
+					std::thread::sleep(WAIT);
+				}
+			};
+		};
+
+		if let Err(_) =
+			telnet.write(format!("memory {:#x} 8 {:#x}\r\n", base + address, 1).as_bytes())
+		{
+			continue;
+		}
+
+		while let Ok(event) = telnet.read_timeout(Duration::from_secs(1)) {
+			match event {
+				TelnetEvent::Data(data) => {
+					let data = String::from_utf8_lossy(&*data);
+					eprint!("{:?}", data);
+					if data.contains("memory OK") {
+						eprint!(" OK");
+						continue 'next_address;
+					}
+				}
+				TelnetEvent::UnknownIAC(_) => {}
+				TelnetEvent::Negotiation(_, _) => {}
+				TelnetEvent::Subnegotiation(_, _) => {}
+				TelnetEvent::TimedOut => {}
+				TelnetEvent::NoData => {}
+				TelnetEvent::Error(_) => {}
+			}
+		}
+	}
 }
